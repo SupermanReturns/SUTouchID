78bc88663b804225e7857868f3b802ec2d4c8822
Superman <703353105@qq.com>
2018-06-06 16:36:53 +0800

    Initial Commit

---
+++ SUTouchID/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SUTouchID
+//
+//  Created by Superman on 2018/6/6.
+//  Copyright © 2018年 Superman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SUTouchID/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SUTouchID
+//
+//  Created by Superman on 2018/6/6.
+//  Copyright © 2018年 Superman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
