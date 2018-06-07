801af35ff87810d0a65934fc9695cd4bcdfa1e99
Superman <703353105@qq.com>
2018-06-07 09:40:37 +0800

    first commit

---
+++ SUTouchID/STouchID.h
@@ -0,0 +1,13 @@
+//
+//  STouchID.h
+//  SUTouchID
+//
+//  Created by Superman on 2018/6/6.
+//  Copyright © 2018年 Superman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STouchID : LAContext
+
+@end
---
+++ SUTouchID/STouchID.m
@@ -0,0 +1,13 @@
+//
+//  STouchID.m
+//  SUTouchID
+//
+//  Created by Superman on 2018/6/6.
+//  Copyright © 2018年 Superman. All rights reserved.
+//
+
+#import "STouchID.h"
+
+@implementation STouchID
+
+@end
